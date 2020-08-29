27f76ba8efda5dc07fee9d3bda1346d1475e0e53
RollLife <qudgh7145@naver.com>
2020-08-29 21:31:21 +0900

    ADD: 나도코딩 숫자 자료형 강의 로직 추가

---
+++ python/basic/1.DataType.py
@@ -0,0 +1,13 @@
+"""
+본 로직은 나도코딩 자료형 파트를 따라서 작성함을 알립니다.
+https://www.youtube.com/watch?v=kWiCuklohdY&t=64s
+"""
+
+# 숫자 자료형
+print(5)  # 정수
+print(-10)  # 음수
+print(3.14)  # 실수
+print(1000)  # 큰 숫자
+print(5 + 3)  # 연산
+print(2 * 8)  # 곱연산
+print(3 * (3 + 1))  # 다소 복잡한(?) 연산
