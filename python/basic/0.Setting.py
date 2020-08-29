0ee6d860e876f9b73c17c863474fe86c04c0a413
RollLife <qudgh7145@naver.com>
2020-08-29 21:25:47 +0900

    ADD: 1. 나도코딩 python 기본 환경설정 세팅

---
+++ python/basic/0.Setting.py
@@ -0,0 +1,10 @@
+"""
+본 로직은 나도코딩 환경설정 파트를 따라서 작성함을 알립니다.
+https://www.youtube.com/watch?v=kWiCuklohdY
+
+저는 본 강의와는 다르게 개발환경을 VisualStudio Code가 아닌 Pycharm을 기준으로 작성하였습니다.
+
+python version은 3.8.5로 세팅해두었습니다.
+"""
+
+print("hello world")
