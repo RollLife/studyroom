396c47d8fef87d969431832b845905117f7feaff
RollLife <qudgh7145@naver.com>
2020-08-29 21:40:31 +0900

    FIX: 나도코딩 Boolean 자료형 추가 및 Output Value Comment 추가

--- python/basic/1.DataType.py
+++ python/basic/1.DataType.py
@@ -3,17 +3,27 @@
 https://www.youtube.com/watch?v=kWiCuklohdY&t=64s
 """
 
-# 숫자 자료형
-print(5)  # 정수
-print(-10)  # 음수
-print(3.14)  # 실수
-print(1000)  # 큰 숫자
-print(5 + 3)  # 연산
-print(2 * 8)  # 곱연산
-print(3 * (3 + 1))  # 다소 복잡한(?) 연산
+# [숫자 자료형]
+print(5)  # >> 5 | 정수
+print(-10)  # >> -10 | 음수
+print(3.14)  # >> 3.14 | 실수
+print(1000)  # >> 1000 | 큰 숫자
+print(5 + 3)  # >> 8 | 연산
+print(2 * 8)  # >> 16 | 곱연산
+print(3 * (3 + 1))  # >> 12 | 다소 복잡한(?) 연산
 
-# 문자형 자료형
-print('풍선')  # single quotation
-print("풍선")  # double quotation
-print("ㅋㅋㅋㅋㅋㅋㅋㅋㅋ")  # continuous string
-print("ㅋ" * 9)  # 위와 같은 결과
+# [문자형 자료형]
+print('풍선')  # >> 풍선 |  single quotation
+print("풍선")  # >> 풍선 |  double quotation
+print("ㅋㅋㅋㅋㅋㅋㅋㅋㅋ")  # >> ㅋㅋㅋㅋㅋㅋㅋㅋㅋ | continuous string
+print("ㅋ" * 9)  # >> ㅋㅋㅋㅋㅋㅋㅋㅋㅋ | 위와 같은 결과(숫자와 문자열을 조합하면 연속된 자료형의 갯수를 나타낼 수 있음)
+
+# [Boolean 자료형]
+
+# 참 / 거짓
+print(5 > 10)  # >> False
+print(5 < 10)  # >> True
+print(True)  # >> True
+print(False)  # >> False
+print(not True)  # >> False
+print(not (5 > 10))  # >> True | 5 > 10 - False => not False == True
