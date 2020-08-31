65ef5783a829f1127c40ff1526a8796e73b50a9d
RollLife <qudgh7145@naver.com>
2020-08-31 21:22:43 +0900

    FIX: add string method

--- python/basic/3.String.py
+++ python/basic/3.String.py
@@ -28,3 +28,23 @@ print("일 : " + jumin[4:6])  # 4 부터 6 직전까지 (4, 5
 print("생년월일 : " + jumin[:6])  # 처음부터 6 직전까지
 print("뒤 7자리 : " + jumin[7:])  # 7번째(1)부터 끝까지
 print("뒤 7자리 (뒤에부터) : " + jumin[-7:])  # 맨 뒤에서 7번째(1)부터 끝까지
+
+# [문자열 처리 함수]
+python = "Python is Amazing"
+print(python.lower())  #
+print(python.upper())
+print(python[0].isupper())
+print(len(python))
+print(python.replace("Python", "Java"))
+
+index = python.index("n")  # 인덱스는 파이썬에서 해당 문자의 위치 0(P),1(y),2(t),3(h),4(o),5(n) -> 5번째 위치(첫번째 n)
+print(index)
+index = python.index("n", index + 1)  # 두번째 n의 위치
+print(index)
+
+# 비슷한 함수 find
+print(python.find("n"))  # index 함수를 썼을 때 처럼 똑같이 실행
+print(python.find("Java"))  # 문자열에서 포함되지 않는 문자가 포함될 경우 -1 반환
+# print(python.index("Java")) # index를 썼을때 포함되지 않는 문자를 확인할 경우 에러 발생
+
+print(python.count("n"))  # python 변수에서 몇개의 n이 확인되었는지 확인해주는 함수
