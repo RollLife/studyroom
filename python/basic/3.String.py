011b8c0820b9e6960524ba1e89513e2e5b7fef09
RollLife <qudgh7145@naver.com>
2020-08-31 21:12:59 +0900

    Create 3.String.py

---
+++ python/basic/3.String.py
@@ -0,0 +1,17 @@
+"""
+본 로직은 나도코딩 문자열 처리 파트를 따라서 작성함을 알립니다.
+https://www.youtube.com/watch?v=kWiCuklohdY
+"""
+
+# [문자열]
+
+sentence = '나는 소년입니다'
+print(sentence)
+sentence2 = "파이썬은 쉬워요"
+print(sentence2)
+sentence3 = """
+나는 소년이고,
+파이썬은 쉬워요
+"""
+print(sentence3)
+# sentence3는 문자열을 길게 혹은 다양하게 표현할때 사용한다. 또한 줄바꿈 문자도 자동으로 포함해준다.
