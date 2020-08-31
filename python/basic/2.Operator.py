f630e3a3428f3c2efda1fd4d88fd00d1e184d584
RollLife <qudgh7145@naver.com>
2020-08-31 20:52:02 +0900

    FIX: 랜덤함수 로직 추가

--- python/basic/2.Operator.py
+++ python/basic/2.Operator.py
@@ -75,3 +75,14 @@ from math import floor, ceil, sqrt  # 다른 라이브러리에서 함수를 가
 print(floor(4.99))  # 4 | 무조건 내림
 print(ceil(3.14))  # 4 | 무조건 올림
 print(sqrt(16))  # 4 | 제곱근
+
+# [랜덤 함수]
+
+from random import *
+
+print(random())  # 0.0 ~ 1.0 미만의 임의의 값 생성
+print((random() * 10))  # 0.0 ~ 10.0 미만의 임의의 값 생성
+print(int(random() * 10))  # 0 ~ 10 미만의 임의의 값 생성
+print(int(random() * 10) + 1)  # 1 ~ 10 이하의 임의의 값 생성
+print(randrange(1, 46))  # 1 ~ 46 미만의 임의의 값 생성
+print(randint(1, 45))  # 1 ~ 45 이하의 임의의 값 생성
