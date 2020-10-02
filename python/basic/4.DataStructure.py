31c41b8fc874a65f6a3fa42b00e9a110a01342dc
RollLife <qudgh7145@naver.com>
2020-10-02 22:04:07 +0900

    Update. dictionary structure

--- python/basic/4.DataStructure.py
+++ python/basic/4.DataStructure.py
@@ -69,3 +69,47 @@ num_list.extend(mix_list)
 print(num_list)
 
 # ==> numlist += mix_list 와 같은 기능
+
+
+# 사전(dictionary) {}
+
+cabinet = {3: "유재석", 100: "김태호"}
+print(cabinet[3])
+print(cabinet[100])
+
+# print(cabinet[5]) >> KEYError
+print(cabinet.get(5))  # >> 값이 없어도 None으로 반환가능
+print(cabinet.get(5, "사용 가능"))  # >> 값이 없을때 default 값 설정 가능
+
+# 사전 자료형 안에 값을 확인 하는 방법
+
+print(3 in cabinet)  # True | 3이라는 key 값이 cabinet 안에 있는지 확인
+print(5 in cabinet)  # False
+
+# 키 값은 정수가 아닌 다른 값으로도 가능
+cabinet = {"A-3": "유재석", "B-100": "김태호"}
+print(cabinet["A-3"])
+print(cabinet["B-100"])
+
+# 새 손님
+print(cabinet)
+cabinet["A-3"] = "김종국"  # 기존 키 값을 다른 값으로 변형 (update)
+cabinet["C-20"] = "조세호"  # 새로운 키 값을 추가 (insert)
+print(cabinet)
+
+# 간 손님
+del cabinet["A-3"]  # 키 값 삭제 (delete)
+print(cabinet)
+
+# key 들만 출력
+print(cabinet.keys())
+
+# value 들만 출력
+print(cabinet.values())
+
+# key, value 쌍으로 출력
+print(cabinet.items())
+
+# 목욕탕 폐점
+cabinet.clear()  # cabinet 안에 존재하는 모든 값들을 삭제
+print(cabinet)
