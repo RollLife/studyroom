83da42cc1167426bc23443ae04a7cb413872ef97
RollLife <qudgh7145@naver.com>
2020-10-02 22:15:31 +0900

    Update. Set Structure

--- python/basic/4.DataStructure.py
+++ python/basic/4.DataStructure.py
@@ -134,3 +134,31 @@ print(menu[1])
 
 (name, age, hobby) = ("김종국", 20, "코딩")
 print(name, age, hobby)
+
+# 셋, 집합 (set)
+# 중복 안됨, 순서 없음
+my_set = {1, 2, 3, 3, 3}
+print(my_set)
+
+java = {"유재석", "김태호", "양세형"}
+python = set(["유재석", "박명수"])
+
+# 교집합 (java 와 python 을 모두 할 수 있는 개발자)
+print(java & python)
+print(java.intersection(python))
+
+# 합집합 (java 할 수 있거나 python 할 수 있는 개발자, 둘 어느거나 할 수 있는 개발자)
+print(java | python)
+print(java.union(python))
+
+# 차집합 (java는 할 수 있지만 python은 할 줄 모르는 개발자)
+print(java - python)
+print(java.difference(python))
+
+# python 할 줄 아는 사람이 늘어남
+python.add("김태호")  # 추가
+print(python)
+
+# java를 까먹은 사람
+java.remove("김태호")  # 값 삭제
+print(java)
