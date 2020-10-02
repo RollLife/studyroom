0df935f6dc192ce0d46a389dcf0dfc29558dfbe2
RollLife <qudgh7145@naver.com>
2020-10-02 22:38:58 +0900

    Update. Data Structure Quiz

--- python/basic/4.DataStructure.py
+++ python/basic/4.DataStructure.py
@@ -175,3 +175,59 @@ print(menu, type(menu))
 
 menu = set(menu)  # 메뉴의 자료 형태를 처음 형태인 set으로 변경
 print(menu, type(menu))
+
+# ----------------------------------------
+"""
+Quiz) 당신의 학교에서는 파이썬 코딩 대회를 주최합니다.
+참석률을 높이기 위해 댓글 이벤트를 진행하기로 하였습니다.
+댓글 작성자들 중에 추첨을 통해 1명은 치킨, 3명은 커피 쿠폰을 받게 됩니다.
+추첨 프로그램을 작성하시오
+
+조건 1: 편의상 댓글은 20명이 작성하였고 아이디는 1~20 이라고 가정
+조건 2: 댓글 내용과 상관 없이 무작위로 추첨히되 중복 불가
+조건 3: random 모듈의 shuffle과 sample을 활용
+
+(출력 예제)
+-- 당첨자 발표 --
+치킨 당첨자 : 1
+커피 당첨자 : [2, 3, 4]
+-- 축하합니다 --
+
+(활용 예제)
+from random import *
+lst = [1,2,3,4,5]
+print(lst)
+shuffle(lst)
+print(lst)
+print(sample(lst, 1))
+"""
+
+# Answer)
+from random import shuffle, sample
+
+id_list = [i for i in range(1, 21)]
+shuffle(id_list)
+win_chick = sample(id_list, 1)[0]
+
+id_list.remove(win_chick)
+
+shuffle(id_list)
+win_coffee = sample(id_list, 3)
+print(f"""-- 당첨자 발표 --
+치킨 당첨자 : {win_chick}
+커피 당첨자 : {win_coffee}
+-- 축하합니다 --""")
+
+# Correct Answer
+from random import *
+
+users = range(1, 21)
+users = list(users)
+
+shuffle(users)
+winners = sample(users, 4)  # 4명 중에서 1명은 치킨, 3명은 커피
+
+print("-- 당첨자 발표 --")
+print(f"치킨 당첨자 : {winners[0]}")
+print(f"커피 당첨자 : {winners[1:]}")
+print("-- 축하합니다 --")
