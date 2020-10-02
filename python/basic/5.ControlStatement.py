283320d3b899682cb78bd81596afdc967eca947c
RollLife <qudgh7145@naver.com>
2020-10-02 23:24:14 +0900

    Update. for Statement

--- python/basic/5.ControlStatement.py
+++ python/basic/5.ControlStatement.py
@@ -17,12 +17,34 @@ elif weather == "미세먼지":
 else:
     print("준비물 필요 없어요.")
 
-temp = int(input("기온은 어떄요? "))
-if 30 <= temp:
-    print("너무 더워요. 나가지마세요.")
-elif 10 <= temp and temp < 30:
-    print("괜찮은 날씨에요")
-elif 0 <= temp < 10:
-    print("외투를 챙기세요")
-else:
-    print("너무 추워요. 나가지마세요")
+# temp = int(input("기온은 어떄요? "))
+# if 30 <= temp:
+#     print("너무 더워요. 나가지마세요.")
+# elif 10 <= temp and temp < 30:
+#     print("괜찮은 날씨에요")
+# elif 0 <= temp < 10:
+#     print("외투를 챙기세요")
+# else:
+#     print("너무 추워요. 나가지마세요")
+
+# for
+# 반복 제어문
+# 같은 작업을 여러번 반복 시킬때 사용
+
+# print("대기번호 : 1")
+# print("대기번호 : 2")
+# print("대기번호 : 3")
+# print("대기번호 : 4")
+
+
+# for waiting_no in [0, 1, 2, 3, 4]:
+#     print("대기번호 : {}".format(waiting_no))
+# for waiting_no in range(5):
+#     print("대기번호 : {}".format(waiting_no))
+for waiting_no in range(1, 6):
+    print("대기번호 : {}".format(waiting_no))
+
+starbucks = ["아이언맨", "토르", "아이엠 그루트"]
+for customer in starbucks:
+    print("{}, 커피가 준비되었습니다".format(customer))
+
