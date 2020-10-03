cc077a48791c2e0508b170e209a6882d909440fe
AhnByeongHo <qudgh7145@naver.com>
2020-10-03 23:14:57 +0900

    Update. Function Call

---
+++ python/basic/6.Fucntion.py
@@ -0,0 +1,15 @@
+"""
+본 로직은 나도코딩 함수 파트를 따라서 작성함을 알립니다.
+https://www.youtube.com/watch?v=kWiCuklohdY
+"""
+
+
+# def (함수 선언)
+# 함수는 특정 부분의 연산이나 작업들을 미리 정의 해두고 호출할때 해당 작업을 실행 할 수 있게 해준다.
+
+
+def open_account():  # 함수 정의
+    print("새로운 계좌가 생성되었습니다.")
+
+
+open_account()  # 함수 호출
