32626158564111a3d80c241f1a32515b3fab19b0
AhnByeongHo <qudgh7145@naver.com>
2020-10-04 16:35:14 +0900

    Update. File Quiz

--- python/basic/7.InputOutput.py
+++ python/basic/7.InputOutput.py
@@ -145,3 +145,34 @@ with open("study.txt", "w", encoding="utf8") as study_file:
 # 읽기
 with open("study.txt", "r", encoding="utf8") as study_file:
     print(study_file.read())
+
+# --------------------------------------------
+"""
+Quiz) 당신의 회사에서는 매주 1회 작성해야 하는 보고서가 있습니다.
+보고서는 항상 아래와 같은 형태로 출력되어야 합니다.
+
+- X 주차 주간보고 -
+부서 : 
+이름 : 
+업무 요약 :
+
+1주차부터 50주차까지의 보고서 파일을 만드는 프로그램을 작성하시오.
+
+조건 : 파일명은 '1주차.txt', '2주차.txt', ... 와 같이 만듭니다.
+"""
+
+# Answer)
+for week in range(1, 51):
+    with open(f"{week}주차.txt", "w", encoding="utf8") as f:
+        f.write(f"- {week} 주차 주간보고 -\n")
+        f.write(f"부서 : \n")
+        print("이름 : ", file=f)
+        print("업무 요약 : ", file=f)
+
+# Correct Answer)
+for i in range(1, 51):
+    with open(str(i) + "주차.txt", "w", encoding="utf8") as f:
+        f.write(f"- {i} 주차 주간보고 -\n")
+        f.write(f"부서 : \n")
+        f.write(f"이름 : \n")
+        f.write(f"업무 요약 : ")
