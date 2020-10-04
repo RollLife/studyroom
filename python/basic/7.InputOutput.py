e1ba65506fb130088aac133d8a7f62ef6f60b1fc
AhnByeongHo <qudgh7145@naver.com>
2020-10-04 16:18:46 +0900

    Update. pickle usage

--- python/basic/7.InputOutput.py
+++ python/basic/7.InputOutput.py
@@ -111,3 +111,19 @@ for line in lines:
     print(line, end="")
 
 score_file.close()
+
+# pickle
+# 프로그램 상에서 사용하고 있는 데이터를 파일 형태로 저장
+# 불러올 때 또한 pickle을 사용해서 파일을 불러올 수 있다.
+import pickle
+
+profile_file = open("profile.pickle", "wb")  # pickle을 쓰기위해서는 항상 binary, encoding은 필요없음
+profile = {"이름": "박명수", "나이": 30, "취미": ["축구", "골프", "코딩"]}
+print(profile)
+pickle.dump(profile, profile_file)  # profile에 있는 정보를 file에 저장
+profile_file.close()
+
+profile_file = open("profile.pickle", "rb") # 읽기 전용, 바이너리
+profile = pickle.load(profile_file) # 파일에 있는 정보를 profile에 불러오기
+print(profile)
+profile_file.close()
