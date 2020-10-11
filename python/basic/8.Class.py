c4fbe9a8835ff7c279bdc440f7dd303bb5d30fca
RollLife <qudgh7145@naver.com>
2020-10-11 17:46:05 +0900

    Update. Multiple Inheritance

--- python/basic/8.Class.py
+++ python/basic/8.Class.py
@@ -185,3 +185,30 @@ firebat1.attack("5시")  # 공격함
 # 두번 공격받음
 firebat1.damaged(25)
 firebat1.damaged(25)
+
+
+# 다중 상속
+# 부모 클래스(Unit, ...)를 두개 이상 상속 받는 것
+# 부모 클래스(Unit), 자식 클래스(AttackUnit) / Unit -(상속)-> AttackUnit
+
+# 드랍쉽 : 공중 유닛, 수송기. 마린 / 파이어뱃 / 탱크 등을 수송
+
+# 날 수 있는 기능을 가진 클래스
+class Flyable:
+    def __init__(self, flying_speed):
+        self.flying_speed = flying_speed
+
+    def fly(self, name, location):
+        print("{} : {} 방향으로 날아갑니다. [속도 {}]".format(name, location, self.flying_speed))
+
+
+# 공중 공격 유닛 클래스
+class FlyableAttackUnit(AttackUnit, Flyable):  # >> AttackUnit, Flyable 두 개의 부모클래스를 상속받음.
+    def __init__(self, name, hp, damage, flying_speed):
+        AttackUnit.__init__(self, name, hp, damage)
+        Flyable.__init__(self, flying_speed)
+
+
+# 발키리 : 공중 공격 유닛, 한번에 14발 미사일 발사.
+valkrie = FlyableAttackUnit("발키리", 200, 6, 5) # 초기화 및 선언
+valkrie.fly(valkrie.name, "3시", "inheritance")
