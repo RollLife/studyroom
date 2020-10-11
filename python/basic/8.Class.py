aa96714d1af9688d2a59d3f016c32f9d8b5a202b
RollLife <qudgh7145@naver.com>
2020-10-11 19:35:04 +0900

    Update. Class Quiz

--- python/basic/8.Class.py
+++ python/basic/8.Class.py
@@ -547,3 +547,78 @@ for unit in attack_units:
 
 # 게임 종료
 game_over()
+
+print("-" * 50 + "절취선" + "-" * 50)
+"""
+Quiz) 주어진 코드를 활용하여 부동산 프로그램을 작성하시오.
+
+(출력 예제)
+총 3대의 매물이 있습니다.
+강남 아파트 매매 10억 2010년
+마포 오피스텔 전세 5억 2007년
+송파 빌라 월세 500/50 2000년
+
+# 코드
+class House:
+    # 매물 초기화
+    def __init__(self, location, house_type, deal_type, price, completion_year):
+        pass
+
+    # 매물 정보 표시
+    def show_detail(self):
+        pass
+"""
+
+
+# Answer)
+class House:
+    # 매물 초기화
+    def __init__(self, location, house_type, deal_type, price, completion_year):
+        self.location = location
+        self.house_type = house_type
+        self.deal_type = deal_type
+        self.price = price
+        self.completion_year = completion_year
+
+    # 매물 정보 표시
+    def show_detail(self):
+        print("{0} {1} {2} {3} {4}".format(self.location, self.house_type, self.deal_type, self.price,
+                                           self.completion_year))
+
+
+apartment = House("강남", "아파트", "매매", "10억", "2010년")
+officetels = House("마포", "오피스텔", "전세", "5억", "2007년")
+villa = House("송파", "빌라", "월세", "500/50", "2000년")
+
+apartment.show_detail()
+officetels.show_detail()
+villa.show_detail()
+
+
+# Correct Answer)
+class House:
+    # 매물 초기화
+    def __init__(self, location, house_type, deal_type, price, completion_year):
+        self.location = location
+        self.house_type = house_type
+        self.deal_type = deal_type
+        self.price = price
+        self.completion_year = completion_year
+
+    # 매물 정보 표시
+    def show_detail(self):
+        print(self.location, self.house_type, self.deal_type, self.price, self.completion_year)
+
+
+houses = []
+house1 = House("강남", "아파트", "매매", "10억", "2010년")
+house2 = House("마포", "오피스텔", "전세", "5억", "2007년")
+house3 = House("송파", "빌라", "월세", "500/50", "2000년")
+
+houses.append(house1)
+houses.append(house2)
+houses.append(house3)
+
+print("총 {}대의 매물이 있습니다.".format(len(houses)))
+for house in houses:
+    house.show_detail()
