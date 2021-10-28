604d2e8826a50c5c57d39f8b06dc4518dca1d978
AhnByeongHo <qudgh7145@naver.com>
2021-10-28 10:03:43 +0900

    ADD: basic part add

---
+++ python/fastcampus_python_all_in_one/basic_part.py
@@ -0,0 +1,13 @@
+"""
+기본 문법을 다루는 파트
+
+아는 부분을 제외하고 내가 새롭게 알게되었거나 필요한 부분만 기재한다.
+"""
+
+print("T", "E", "S", "T", sep="")
+
+print("Roll", "Life", sep="")
+print("blabla", "@naver.com", sep="@")
+
+print("예아", end="")
+print("이것은 무엇이지")
