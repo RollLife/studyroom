d18d46de79a95e08ad2d890cc965670b5a50b7c1
AhnByeongHo <qudgh7145@naver.com>
2021-10-28 10:11:14 +0900

    FIX: print 출력방법

--- python/fastcampus_python_all_in_one/basic_part.py
+++ python/fastcampus_python_all_in_one/basic_part.py
@@ -4,6 +4,7 @@
 아는 부분을 제외하고 내가 새롭게 알게되었거나 필요한 부분만 기재한다.
 """
 
+# print
 print("T", "E", "S", "T", sep="")
 
 print("Roll", "Life", sep="")
@@ -11,3 +12,8 @@ print("blabla", "@naver.com", sep="@")
 
 print("예아", end="")
 print("이것은 무엇이지")
+
+print("{} {} ".format("ㅁㄴㅇㄹ", "ㅁㅇㄴㄹ"))
+print("{0} {1}".format(123, 123))
+print("{0: 5d} {1: 5.4f}".format(123, 123.123123))
+print("{a: 5d} {b: 5.4f}".format(a = 123, b =123.123123))
