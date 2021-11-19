490ecfdd060e539909be238341096723c7e42278
AhnByeongHo <qudgh7145@naver.com>
2021-11-19 17:20:04 +0900

    ADD: create django app tutorial part 1

--- python/django-tutorial/mysite/mysite/urls.py
+++ python/django-tutorial/mysite/mysite/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
+# include always use in every urls, if admin.site.urls only exclude
 urlpatterns = [
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
