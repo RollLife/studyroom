3d2125e964a9ff26464ee75e043130787ec12bae
AhnByeongHo <qudgh7145@naver.com>
2021-11-19 17:07:33 +0900

    ADD: write run command line in shellscript

---
+++ python/django-tutorial/executed-command-line.sh
@@ -0,0 +1,13 @@
+# shellcheck disable=SC2164
+
+# init virtualenv & install django
+poetry init
+poetry install
+poerty add django==3.2
+
+# init django
+django-admin startproject mysite
+
+# test django successful running
+cd mysite
+python manage.py runserver
